ee6ee9df7aeb78779d3d618a01420b0b45302d5d
Luca Sguanci <luca.sguanci@gmail.com>
2025-01-28 15:54:26 +0100

    Update lab27--turn-on-neopixel.py

--- code/lab27--turn-on-neopixel.py
+++ code/lab27--turn-on-neopixel.py
@@ -2,3 +2,17 @@ import time
 import board
 import digitalio
 import neopixel
+
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.20, auto_write=False)
+
+while True:
+  # set color red on NeoPixel and turn it on
+  color = (255, 0, 0)
+  dot.fill(color)
+  dot.show()
+  time.sleep(1)
+  # turn off neopixel, i.e. show black
+  color = (0, 0, 0)
+  dot.fill(color)
+  dot.show()
+  time.sleep(1)
